ee0d0ef9ffcadf568da762f926c01fb55ae076b0
Karl <karlhinterseber1@gmail.com>
2021-04-21 10:43:22 +0200

    feat: complete issues 2 fixes #2

--- drive.js
+++ drive.js
@@ -4,13 +4,18 @@ const fs = require ('fs/promises');
 const tmp = "C:\\Users\\karlo\\AppData\\Local\\Temp\\nodeTest";
 
 
-function listfolder () {
-    const promise = fs.readdir(tmp, {withFileTypes:true})
+function listfolder (file='') {
+    //retourne une promesse sous forme de tableau pour lire le dossier avec fs.readdir
+    const promise = fs.readdir(tmp+'\\'+file, {withFileTypes:true})
+    //faire une promesse qui en cas de succès renvoie un tableau
     const transfpromise = promise.then((results) => {
+        //création du tableau vide
         const tab =[];
+        //parcours le tableau et rajoute dans celui-ci un fichier ou un dossier
         results.forEach(function(resultobj) {
             tab.push({
                 name: resultobj.name,
+                //pour voir si c'est un dossier on utilise isDirectory
                 isFolder: resultobj.isDirectory()
             })
         })
@@ -19,6 +24,21 @@ function listfolder () {
     return transfpromise;
 }
 
+
+//Retourne le contenu de {name}
+function returnContent (file) {
+        const promiseUn = fs.readFile(tmp+'\\'+file, {encoding:'utf-8'})
+    return promiseUn;
+}
+
+function folderOrFile (file) {
+    const promiseDeux = fs.stat(tmp+'\\'+file)
+    return promiseDeux;
+}
+
+
 module.exports = {
-    list: listfolder
+    returnContent: returnContent,
+    list: listfolder,
+    folderOrFile: folderOrFile
 }
